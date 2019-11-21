09f976afa59b8b9db5387e256102a696fdd69709
Kenneth Pham <kpham1@mymail.mines.edu>
2019-11-20 22:43:41 -0700

    created file that chagnes the letter at a desired position

--- Week12-utility.py
+++ Week12-utility.py
@@ -10,3 +10,9 @@ def LoadFile(File_Name):
     with open(File_Name, 'r') as f:
         return [line.strip('\n') for line in f]
 
+def UpdateString(string, character, position):
+    string = list(string)
+    string[position] = character
+    result = ''.join(string)
+    print(result)
+
